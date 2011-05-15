b2a26cd71089fe2b14df655c1ba5d5fff0da50c4
Sikevux <sikevux@sikevux.se>
2011-05-15 19:59:26 +0200

    Added header stuff

--- Test.py
+++ Test.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
 """Old school testing"""
 from Main import FTPConnector, ConnectionInfo
 slist = [ConnectionInfo("192.168.1.100", "dummy", "server")]
