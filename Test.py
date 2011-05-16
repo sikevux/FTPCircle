9fe05eae0ccccde7a0b5b5a1fae3da472095efd7
Sikevux <sikevux@sikevux.se>
2011-05-16 19:42:27 +0200

    Started on #10 and made a very simple unit testing class

--- Test.py
+++ Test.py
@@ -2,10 +2,26 @@
 # -*- coding: utf-8 -*-
 # vim:fileencoding=utf8
 
-"""Old school testing"""
+import unittest
 from Main import FTPConnector, ConnectionInfo, ServerList
-m = ServerList()
-slist = m.make_array()
-t = FTPConnector(slist)
-t.list()
-"""Soon to be unit testing class"""
+
+class TestSequence(unittest.TestCase):
+	""" Class to do all the testing"""
+	def setUp(self):
+		""" Things that will need to be run before the tests"""
+		self._server_list = ServerList()
+		self._server_list_array = self._server_list.make_array()
+
+	def test_connect(self):
+		self._ftp_connector = FTPConnector(self._server_list_array)
+
+	def test_list(self):
+		self._ftp_connector = FTPConnector(self._server_list_array)
+		self._ftp_connector.list()
+
+	def test_discconect(self):
+		self._ftp_connector = FTPConnector(self._server_list_array)
+		self._ftp_connector.disconnect()
+
+if __name__ == '__main__':
+	unittest.main()
