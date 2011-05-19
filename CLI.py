14e866479ca936bc91407639a44762d8dd9fe2ff
Sikevux <sikevux@sikevux.se>
2011-05-19 07:14:36 +0200

    Started drafting on #22

--- CLI.py
+++ CLI.py
@@ -4,14 +4,29 @@
 
 import sys
 import getopt
+from Main import FTPConnector, ConnectionInfo, ServerList
 
+
+class BasicCLI():
+	"""Basic CLI"""
+	def __init__(self):
+		self._server_list = ServerList()
+		self._server_list_matrix = self._server_list.make_matrix()
+		self._ftp_connector = FTPConnector(self._server_list_matrix)
+		while True:
+			moo = raw_input(">> ")
+			if moo == "list":
+				self._ftp_connector.list()
+			elif moo in ("exit", "quit"):
+				sys.exit()
+			print moo
 
 class ArgumentHandler():
 	"""Usage: sumfile [-h help] [-d debug] [-s serverlist]"""
 	def usage(self):
 		""" Handles the usage"""
 		#TODO Fix better usage
-y		print self.__doc__
+		print self.__doc__
 
 	def help_arg(self):
 		"""Handles the help flag"""
@@ -37,9 +52,13 @@ y		print self.__doc__
 				self.debug_arg()
 			elif opt == "-s":
 				#TODO Handle serverlist
-				
+				print "m00"
+
 def main(argv):
 	""" Tralalalal """
 	ArgumentHandler(argv)
 if __name__ == "__main__":
 	main(sys.argv[1:])
+
+
+
---
+++ CLITest.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf8
+
+from CLI import BasicCLI
+
+BasicCLI()
+
